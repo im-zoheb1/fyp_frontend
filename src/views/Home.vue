87c849b3556b5463d61d910753ea6a8c3d5df97c
Zuhaib Khan <xuhaib002@gmail.com>
2021-05-16 20:14:45 +0500

    before webpack

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div :class="[$style.header]">something</div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+
+<style lang="scss" module>
+.header {
+    background-color: var(--primary-color);
+}
+</style>
