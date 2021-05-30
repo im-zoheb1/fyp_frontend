d5f8fe64e6d7119dd5ec96671f10f27d2f317e28
Zuhaib Khan <xuhaib002@gmail.com>
2021-05-30 12:10:42 +0500

    fixing axios issue

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -5,7 +5,7 @@ _axios.defaults.baseURL = "https://ccidentifier-rest-api.herokuapp.com";
 _axios.defaults.timeout = "20000";
 _axios.defaults.headers.common[
   "Authorization"
-] = `Bearer ${localStorage.getItem("token")}`;
+] = `Bearer ${localStorage.getItem("refresh_token")}`;
 
 window.$axios = _axios;
 
